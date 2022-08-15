25039ae4c699f62ea1e9bc1f66824b6dd684d14b
epetrishchev <epetrishchev@edispa.com>
2022-08-16 00:49:48 +0400

    Добавил модель Rubric, добавил навигацию на главной по рубрикам

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Bb
+from .models import Bb, Rubric
 
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ["title", "content", "price", "published"]
-    list_display_links = ["title", "content"]
-    search_fields = ["title", "content"]
+    list_display = ['title', 'content', 'price', 'rubric', 'published']
+    list_display_links = ['title', 'content']
+    search_fields = ['title', 'content']
+
+
+class RubricAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
 
 
 admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric, RubricAdmin)
--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField("Товар", max_length=50)
-    content = models.TextField("Описание", null=True, blank=True)
-    price = models.FloatField("Цена", null=True, blank=True)
-    published = models.DateTimeField("Опубликовано", auto_now_add=True, db_index=True)
+    title = models.CharField('Товар', max_length=50)
+    content = models.TextField('Описание', null=True, blank=True)
+    price = models.FloatField('Цена', null=True, blank=True)
+    published = models.DateTimeField('Опубликовано', auto_now_add=True, db_index=True)
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
     class Meta:
-        verbose_name_plural = "Объявления"
-        verbose_name = "Объявление"
-        ordering = ["-published"]
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField('Название', max_length=20, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['-name']
