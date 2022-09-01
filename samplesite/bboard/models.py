52b1defe2e440ed22ed833dab5939bee1cdf55a0
epetrishchev <epetrishchev@edispa.com>
2022-09-01 23:10:07 +0300

    Добавил базовый шаблон и статические файлы

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,17 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField('Товар', max_length=50)
-    content = models.TextField('Описание', null=True, blank=True)
-    price = models.FloatField('Цена', null=True, blank=True)
-    published = models.DateTimeField('Опубликовано', auto_now_add=True, db_index=True)
-    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+    title = models.CharField(
+        'Товар',
+        max_length=50
+    )
+    content = models.TextField(
+        'Описание',
+        null=True,
+        blank=True
+    )
+    price = models.FloatField(
+        'Цена',
+        null=True,
+        blank=True
+    )
+    published = models.DateTimeField(
+        'Опубликовано',
+        auto_now_add=True,
+        db_index=True
+    )
+    rubric = models.ForeignKey(
+        'Rubric',
+        null=True,
+        on_delete=models.PROTECT,
+        verbose_name='Рубрика'
+    )
 
     class Meta:
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published']
+        # Определяем сочетания столбцов модели, значения которых должны быть уникальными.
+        unique_together = (
+            ('title', 'published'),
+            ('title', 'price', 'rubric')
+        )
+        get_latest_by = 'published'
 
 
 class Rubric(models.Model):
@@ -24,3 +51,8 @@ class Rubric(models.Model):
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
         ordering = ['-name']
+
+
+class AdvUser(models.Model):
+    is_activate = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
